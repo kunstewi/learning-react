7b6933a3ff689069c85be42ab56ec5c11cc1a85c
kunstewi <kan.ai35423@gmail.com>
2025-10-27 13:04:45 +0530

    initial structure for learning react

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+// First way arrow function bottom export
+// const App = () => {
+//   return (
+//     <div>App</div>
+//   )
+// }
+// export default App;
+
+// Second way inline export
+// export const App = () => {
+//   return (
+//     <div>App</div>
+//   )
+// }
+
+// Third way (Normal function)
+// export default function App(){
+//   return(
+//     <div>App</div>
+//   )
+// }
+
+const App = () => {
+  return <div>App</div>;
+};
+export default App;
